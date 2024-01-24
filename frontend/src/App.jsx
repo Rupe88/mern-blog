fc1b46964e3d5716bf063034aa15079d9df46f83
Rupe88 <chyrupesh828@gmail.com>
2024-01-24 08:06:18 +0545

    create pages ans routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,21 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
 const App = () => {
   return (
     <>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/sign-in" element={<Signin/>}/>
+      <Route path="/sign-up" element={<Signup/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
+    </Routes>
+    </BrowserRouter>
     
     </>
   )
